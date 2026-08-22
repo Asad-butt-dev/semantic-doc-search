b93fd4477e8cfdba46b3a6ecf7b535196dbc19d7
Asad Butt <318744266+Asad-butt-dev@users.noreply.github.com>
2026-08-22 11:38:43 +0200

    adss a searching label and fixes problem 422 by renaming the parameters of API and request

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,Query
-from search import searches
+from search import search_chunks
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -10,7 +10,7 @@ app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,a
 
 @app.get("/search")
 def search(query:str,use_hybrid:bool,chunk_size:int=Query(default=500,ge=199,le=651),bonus:float=Query(default=0,ge=0,le=0.6),key_terms:list[str]=Query(default=[]),):
-    return searches(query,key_terms,use_hybrid,chunk_size,bonus)
+    return search_chunks(query,key_terms,use_hybrid,chunk_size,bonus)
 
 
 @app.get("/health")
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,17 +16,19 @@ function App() {
   const [terms, setTerms] = useState("")
   const [results, setResults] = useState<SearchResult[]>([]);
   const [bonus, setBonus] = useState("0");
-
+  const [loading,setLoading]= useState(false)
   async function handleSearch() {
+    setLoading(true);
     const termList = terms.split(",").map(a => a.trim())
-    const params = new URLSearchParams({ qu: query, hs: String(hybrid), ch: chunkSize, bo: bonus })
+    const params = new URLSearchParams({ query: query, use_hybrid: String(hybrid), chunk_size: chunkSize, bonus: bonus })
     for (let i = 0; i < termList.length; i++) {
-      params.append("te", termList[i]);
+      params.append("key_terms", termList[i]);
     }
     const url = "http://127.0.0.1:8000/search?" + params
     const answer = await fetch(url);
     const data = await answer.json();
     setResults(data);
+    setLoading(false);
   }
 
   return (
@@ -36,8 +38,8 @@ function App() {
       <input type="checkbox" checked={hybrid} onChange={e => setHybrid(e.target.checked)} /> <label>HybridSearch</label>
       <input type='text' placeholder='Terminology' value={terms} onChange={e => setTerms(e.target.value)} />
       <input type="text" placeholder='bonus' value={bonus} onChange={e => setBonus(e.target.value)} />
-      <button onClick={handleSearch}> Search </button>
-      <div>{results.map(r => (<div key={`${r.fileName}-${r.chunkNumber}`}><h3>{r.fileName} Chunk: {r.chunkNumber} cosinus similarity:{r.score.toFixed(3)} bonus +{r.bonus} </h3><p> {r.text.slice(0, 300)}</p> </div>))} </div>
+      <button onClick={handleSearch} disabled={loading}> Search </button>
+      <div> {loading ? <p>Searching...</p>: results.map(r => (<div key={`${r.fileName}-${r.chunkNumber}`}><h3>{r.fileName} Chunk: {r.chunkNumber} cosinus similarity:{r.score.toFixed(3)} bonus +{r.bonus} </h3><p> {r.text.slice(0, 300)}</p> </div>))} </div>
     </div>
   )
 }
