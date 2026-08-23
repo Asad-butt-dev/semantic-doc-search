de7fdad3d0a52e72822ce297b6e7629324e98f3b
Asad Butt <318744266+Asad-butt-dev@users.noreply.github.com>
2026-08-23 12:46:03 +0200

    Add Stack and Paper components for visualization. removed content of App.css, index.css(Conflict with Mamtine)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
 import { useState } from 'react'
-import { TextInput, Button, Checkbox } from '@mantine/core';
+import { TextInput, Button, Checkbox, Stack, Paper } from '@mantine/core';
 interface SearchResult {
   text: String;
-  fileName: String;
-  chunkNumber: number;
+  file_name: String;
+  chunk_number: number;
   score: number;
   bonus: number;
 
@@ -18,7 +18,7 @@ function App() {
   const [results, setResults] = useState<SearchResult[]>([]);
   const [bonus, setBonus] = useState("0");
   const [loading, setLoading] = useState(false)
-  const [error,setError]=useState<string|null>(null);
+  const [error, setError] = useState<string | null>(null);
   async function handleSearch() {
     const termList = terms.split(",").map(a => a.trim())
     const params = new URLSearchParams({ query: query, use_hybrid: String(hybrid), chunk_size: chunkSize, bonus: bonus })
@@ -26,42 +26,49 @@ function App() {
       params.append("key_terms", termList[i]);
     }
     const url = "http://127.0.0.1:8000/search?" + params;
-  
+    
     try {
       setLoading(true);
       setError(null)
       const answer = await fetch(url);
-      if (answer.status != 200) {
+      if (!answer.ok ) {
         throw new Error(answer.status.toString())
       }
-    const data = await answer.json();
-    setResults(data);
+      const data = await answer.json();
+      setResults(data);
 
     } catch (error) {
-     setError("Could not reach the server");
-     console.log(error);
-    setResults([]);
+      setError(String(error));
+      console.log(error);
+      setResults([]);
     }
-    finally{
+    finally {
 
-    setLoading(false);
+      setLoading(false);
     }
   }
 
   return (
-    <div>
-      <TextInput value={query}  onChange={e => setQuery(e.target.value)} placeholder="Search..."
-  label="Query"/> 
-     <Checkbox checked={hybrid} onChange={e=>setHybrid(e.target.checked)} placeholder='Hybrid' label="HybridSearch" />
-      <TextInput value={chunkSize} onChange={e=>setChunkSize(e.target.value)} placeholder="Chunksize" label="Chunksize"/> 
-      <TextInput value={terms} onChange={e=>setTerms(e.target.value)} placeholder='Terminology' label="Terminilogy"  />
-      <TextInput  value={bonus} onChange={e=>setBonus(e.target.value)} placeholder="Bonus" label="Bonus"  />
-      <Button onClick={handleSearch} disabled={!query.trim()||loading}> Search</Button>
-      <div> {loading ? <p>Searching...</p> : results.map(r => (<div key={`${r.fileName}-${r.chunkNumber}`}><h3>{r.fileName} Chunk: {r.chunkNumber} cosinus similarity:{r.score.toFixed(3)} bonus +{r.bonus} </h3><p> {r.text.slice(0, 300)}</p> </div>))} </div>
-    { error && <p  role="alert" style={{color:"red"}}> {error} </p>}
-     
-    </div>
-    
+
+
+    <Stack 
+      gap="md">
+        <TextInput value={query} onChange={e => setQuery(e.target.value)} placeholder="Search..."
+        label="Query" />
+      <Checkbox checked={hybrid} onChange={e => setHybrid(e.target.checked)} placeholder='Hybrid' label="HybridSearch" />
+      <TextInput value={chunkSize} onChange={e => setChunkSize(e.target.value)} placeholder="Chunksize" label="Chunksize (200-650)" />
+      <TextInput value={terms} onChange={e => setTerms(e.target.value)} placeholder='Terminology' label="Terminology" />
+      <TextInput value={bonus} onChange={e => setBonus(e.target.value)} placeholder="Bonus" label="Bonus" />
+
+      <Button onClick={handleSearch} disabled={!query.trim() || loading}> Search</Button>
+
+      <div> {loading ? <p>Searching...</p> : results.map(r => (<Paper withBorder shadow="xl" radius="md" p="md"  key={`${r.file_name}-${r.chunk_number}`}><h3>{r.file_name} Chunk: {r.chunk_number} cosinus similarity:{r.score.toFixed(3)} bonus +{r.bonus} </h3><p> {r.text.slice(0, 300)}</p> </Paper>))} </div>
+      {error && <p role="alert" style={{ color: "red" }}> {error} </p>}
+    </Stack>
+
+
+
+
   )
 }
 
