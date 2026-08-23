8d1f3dbb54e6061034045f93b690111b0d6067f5
Asad Butt <318744266+Asad-butt-dev@users.noreply.github.com>
2026-08-23 19:56:13 +0200

    improve results  display

--- frontend/src/ResultList.tsx
+++ frontend/src/ResultList.tsx
@@ -1,7 +1,7 @@
 import type { SearchResult } from "./types";
 import { Paper,Title,Text ,Group, Badge} from "@mantine/core"
 function ResultList({ results }: { results: SearchResult[] }) {
-    return  results.map(r => (<Paper withBorder shadow="xl" radius="md" p="md" key={`${r.file_name}-${r.chunk_number}`}><Group justify="space-between"><Title order={4}>{r.file_name} Chunk: {r.chunk_number}</Title> </Group> cosinus similarity: <Badge>{ r.score.toFixed(3)}</Badge> {r.bonus!==undefined && ( <> <Text>bonus+</Text><Badge color="green">+{r.bonus.toFixed(3)}</Badge></>) }  <Text size={"sm"} c={"dimmed"} lineClamp={3}> {r.text.slice(0, 300)}</Text> </Paper>))
+    return  results.map(r => (<Paper withBorder shadow="xl" radius="md" p="md" key={`${r.file_name}-${r.chunk_number}`}><Group justify="space-between"><Title order={4}>{r.file_name} Chunk: {r.chunk_number}</Title> </Group> cosinus similarity: <Badge>{ r.score.toFixed(3)}</Badge> bonus+ {r.bonus!==undefined && ( <Badge color="green">{r.bonus.toFixed(3)}</Badge>) }  <Text size={"sm"} c={"dimmed"} lineClamp={3}> {r.text.slice(0, 300)}</Text> </Paper>))
 }
 
 
