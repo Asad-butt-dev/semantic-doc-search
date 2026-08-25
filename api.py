d40997fbd4f6ca0e3d32ebf82a7d59b9eb08f38c
Asad Butt <318744266+Asad-butt-dev@users.noreply.github.com>
2026-08-25 15:54:18 +0200

    update bonus range

--- api.py
+++ api.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI,Query
 from search import search_chunks
 from fastapi.middleware.cors import CORSMiddleware
 import os 
+from read import DEFAULT_CHUNK_SIZE
 
 app = FastAPI()
 origins=os.environ.get("ALLOWED_ORIGINS","http://localhost:5173").split(",")
 app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
 @app.get("/search")
-def search(query:str,use_hybrid:bool,chunk_size:int=Query(default=500,ge=199,le=651),bonus:float=Query(default=0,ge=0,le=0.6),key_terms:list[str]=Query(default=[]),):
-    return search_chunks(query,key_terms,use_hybrid,chunk_size,bonus)
+def search(query:str,use_hybrid:bool,bonus:float=Query(default=0,ge=0,le=0.4),key_terms:list[str]=Query(default=[]),):
+    return search_chunks(query,key_terms,use_hybrid,DEFAULT_CHUNK_SIZE,bonus)
 
 
 @app.get("/health")
